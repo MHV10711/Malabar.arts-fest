62f0eac2b9b6e855b5e055f70b1b7b03040dd233
MHV10711 <hameemvallappil41@gmail.com>
2026-09-18 13:50:28 +0530

    Format supabase.js for better readability

--- supabase.js
+++ supabase.js
@@ -1,9 +1,11 @@
-const SUPABASE_URL = "https://jlitghsdscahpxjfywnr.supabase.co";
+const SUPABASE_URL =
+    "https://jlitghsdscahpxjfywnr.supabase.co";
 
 const SUPABASE_KEY =
-    "sb_publishable_Nwa9_cZlhLpEPCOERsYeQw_JgROWSPW";
+    "sb_publishable_Nwa9_cZlhLpEPCOERsYQw_JgROWSPW";
 
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
