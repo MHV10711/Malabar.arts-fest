3f289513e0e92af52039d7d1709ce9e74e12c4f9
MHV10711 <hameemvallappil41@gmail.com>
2026-09-18 12:20:16 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://jlitghsdscahpxjfywnr.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_Nwa9_cZlhLpEPCOERsYeQw_JgROWSPW";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
