ff2f7ca4379fa7c35697ff551e310b5dffd9c2a6
MHV10711 <hameemvallappil41@gmail.com>
2026-09-19 11:41:07 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,16 +1,9 @@
-/* =========================================================
-   MIS ART FEST 2026
-   SUPABASE CONNECTION
-   ========================================================= */
-
-const SUPABASE_URL =
-    "https://jlitghsdscahpxjfywnr.supabase.co";
+const SUPABASE_URL = "https://jlitghsdscahpxjfywnr.supabase.co";
 
 const SUPABASE_KEY =
-    "sb_publishable_Nwa9_cZlhLpEPCOERsYeQw_JgROWSPW";
+    "sb_publishable_Nwa9_cZlhLpEPCOERsYQw_JgROWSPW";
 
-const supabaseClient =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_KEY
-    );
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
